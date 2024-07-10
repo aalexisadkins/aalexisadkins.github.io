d3db4e9c27747e4c19270ec16270a17bf58f2a51
“aalexisadkins <ana04657@uga.edu>
2024-07-09 22:50:44 -0400

    trying to reroute

--- aa-portfolio/src/App.js
+++ aa-portfolio/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/aalexisadkins.github.io">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
