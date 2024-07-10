4c3e07ec3bf81379b9c5892834d3ec84a543bf3c
“aalexisadkins <ana04657@uga.edu>
2024-07-09 22:58:48 -0400

    maybe this time

--- aa-portfolio/src/App.js
+++ aa-portfolio/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/aalexisadkins.github.io">
+    <Router basename="/">
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
